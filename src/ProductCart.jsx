5c14125b0057f7f178a3218007b6738373e55c10
luismartinez205 <luisfrancisco198@gmail.com>
2025-04-06 10:39:38 -0500

    Refactor ProductCard component: update card body layout for improved alignment and spacing

--- src/ProductCart.jsx
+++ src/ProductCart.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product, addToCart }) => {
      
       <div className="card m-2" style={{ width: '18rem' }} >
         <img src={product.image} className="card-img-top img-consistente" alt={product.name} width="300" />
-        <div className="card-body">
+        <div className="card-body d-flex flex-column align-items-stretch justify-content-end">
           <h5 className="card-title">{product.name}</h5>
           <p className="card-text text-secondary">{product.description}</p>
           <p className="card-text"><strong>Precio:</strong> ${product.price}</p>
